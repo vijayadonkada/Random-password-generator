922e7ca5fe88737ccad35a7a14310e7a90720e29
vijayadonkada <vijaya9donkada@gmail.com>
2024-05-17 10:12:53 +0530

    Add files via upload

---
+++ passwordgen.py
@@ -0,0 +1,9 @@
+import random
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%&"
+string = lower + upper + numbers + symbols
+length = 16
+password = " ".join(random.sample(string,length))
+print("Your password is:" + password)
